063367d7805186539e45580f4dcc26df26d34de1
Himanshu Mishra <himanshu.m@codestoresolutions.com>
2023-04-03 12:13:08 +0530

    Count Frequency of character

---
+++ Character Frequency/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Character_Frequency
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            Count count = new();
+            try
+            {
+                count.CheckFrequency(s);
+                count.ShowFrequency();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+            Console.ReadKey();
+        }
+        class Count
+        {
+            int[] frequency;
+            public Count()
+            {
+                this.frequency = new int[256];
+            }
+            public void CheckFrequency(string sentence)
+            {
+                if (sentence.Length < 1)
+                {
+                    throw new ArgumentException("Invalid String ...");
+                }
+                for(int i=0; i < sentence.Length; i++)
+                {
+                    frequency[sentence[i]]++;
+                }
+            }
+            public void ShowFrequency()
+            {
+                Console.WriteLine("The Frequency of Characters are : ");
+                for(int index=0; index< 256; index++)
+                {
+                    if (this.frequency[index] > 0)
+                    {
+                        Console.WriteLine(Convert.ToChar(index) + " -> " + this.frequency[index]);
+                    }
+                }
+                    
+            }
+           
+        }
+    }
+}
